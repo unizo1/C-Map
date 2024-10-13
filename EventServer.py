16123b8de5b1110bf464c76e46ba767b403589b5
Kbarr <kbarr4@gmu.edu>
2024-10-12 22:57:17 -0400

    Changed key to input in EventServer

--- EventServer.py
+++ EventServer.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def personal_events():
-    searchword = request.args.get('key', '')
+    searchword = request.args.get('input', '')
     output = EventAi(searchword)
     return output
